1d1af4bdcefa258cfb69b34a159701ab5c16ef12
Rifqi-Dev <firlianrifqi22@gmail.com>
2024-05-09 21:13:46 +0700

    push to prod

--- src/service/analyticService.js
+++ src/service/analyticService.js
@@ -2,8 +2,8 @@ import axios from "axios";
 // import { configDotenv } from "dotenv";
 // configDotenv();
 
-// const API_URL = "https://api.kapuyuaxdev.my.id/";
-const API_URL = "http://localhost:3600/";
+const API_URL = "https://api.kapuyuaxdev.my.id/";
+// const API_URL = "http://localhost:3600/";
 
 export const fetchAnalytic = async () => {
   // console.log(API_URL);
