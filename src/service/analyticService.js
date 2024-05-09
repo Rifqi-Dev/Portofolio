4494b7afd8f6a41bf677cf2b7e3fe2506b187287
Rifqi-Dev <firlianrifqi22@gmail.com>
2024-05-09 17:39:18 +0700

    fix cookie

--- src/service/analyticService.js
+++ src/service/analyticService.js
@@ -2,9 +2,10 @@ import axios from "axios";
 // import { configDotenv } from "dotenv";
 // configDotenv();
 
-const API_URL = "https://api.kapuyuaxdev.my.id/";
+// const API_URL = "https://api.kapuyuaxdev.my.id/";
+const API_URL = "http://localhost:3600/";
 
 export const fetchAnalytic = async () => {
   // console.log(API_URL);
-  return axios.get(API_URL + "analytics");
+  return axios.get(API_URL + "analytics", { withCredentials: true });
 };
