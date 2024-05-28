e29a07ed34c638e8231133c033dc5313a30866fd
Rifqi-Dev <firlianrifqi22@gmail.com>
2024-05-28 20:28:09 +0700

    add techstack in skill and service

--- src/App.js
+++ src/App.js
@@ -32,13 +32,13 @@ function App() {
         options={template}
       />
       <Intro />
-      <section className="h-screen w-full">
+      <section className="w-full">
         <WhatIdo />
       </section>
-      <div className="text-white flex flex-wrap justify-evenly gap-5 px-10 py-5">
+      {/* <div className="text-white flex flex-wrap justify-evenly gap-5 px-10 py-5">
         <Weather />
         <Earthquacke />
-      </div>
+      </div> */}
       {/* <Resume /> */}
       {/* <div className="glowing_stars">
         <div className="star"></div>
