b3666902d1656f8a152621971e37d4d5a6be2966
Rifqi-Dev <firlianrifqi22@gmail.com>
2024-08-24 14:38:38 +0700

    add loading animation

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useCallback, useEffect } from "react";
+import { useCallback, useEffect, useState } from "react";
 import "./App.css";
 import Intro from "./components/intro";
 import Resume from "./components/resume/Index";
@@ -12,8 +12,11 @@ import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import Projects from "./components/Projects";
 import Navbar from "./components/Navbar";
+import Loading from "./components/Loading/Loading";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [isTransitioning, setIsTransitioning] = useState(false);
   useEffect(() => {
     document.title = "Rifqi Dev ";
   }, []);
@@ -27,75 +30,67 @@ function App() {
   const particlesLoaded = useCallback(async (container) => {
     // await console.log(container);
   }, []);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setIsLoading(false);
+      setIsTransitioning(true);
+    }, 4000);
+
+    return () => clearTimeout(timeout);
+  }, []);
+
+  useEffect(() => {
+    if (!isLoading) {
+      const transitionTimeout = setTimeout(() => {
+        setIsTransitioning(false);
+      }, 100);
+
+      return () => clearTimeout(transitionTimeout);
+    }
+  }, [isLoading]);
   return (
     <div className="App text-white ">
-      <Navbar />
       <Particles
         init={particlesInit}
         loaded={particlesLoaded}
         options={template}
       />
-      <Intro />
-      <section className="w-full my-4  md:min-h-screen " id="skills">
-        <WhatIdo />
-      </section>
+      {isLoading && (
+        <div
+          className={`${
+            isLoading ? "opacity-100" : "opacity-0"
+          } transition-opacity duration-500`}
+        >
+          <Loading />
+        </div>
+      )}
+      <div
+        className={`${
+          !isLoading && !isTransitioning ? "opacity-100" : "opacity-0"
+        } transition-opacity duration-500`}
+      >
+        {!isLoading && !isTransitioning && (
+          <>
+            <Navbar />
+            <Intro />
+            <section className="w-full my-4  md:min-h-screen " id="skills">
+              <WhatIdo />
+            </section>
 
-      <section className="w-full my-4 " id="project">
-        <Projects />
-      </section>
-      <section className="w-full mt-4" id="resume">
-        <Resume />
-      </section>
-      <section className="w-full mt-8" id="contact">
-        <Contact />
-      </section>
-      {/* <div className="text-white flex flex-wrap justify-evenly gap-5 px-10 py-5">
-        <Weather />
-        <Earthquacke />
-      </div> */}
-      <Footer />
-      {/* <div className="glowing_stars">
-        <div className="star"></div>
-        <div className="star"></div>
-        <div className="star"></div>
-        <div className="star"></div>
-        <div className="star"></div>
-        <div className="star"></div>
-        <div className="star"></div>
-        <div className="star"></div>
-        <div className="star"></div>
-        <div className="star"></div>
-        <div className="star"></div>
-        <div className="star"></div>
-        <div className="star"></div>
-        <div className="star"></div>
-        <div className="star"></div>
-        <div className="star"></div>
-        <div className="star"></div>
-        <div className="star"></div>
-        <div className="star"></div>
-        <div className="star"></div>
-        <div className="star"></div>
-        <div className="star"></div>
-        <div className="star"></div>
-        <div className="star"></div>
-        <div className="star"></div>
-        <div className="star"></div>
-        <div className="star"></div>
-        <div className="star"></div>
-        <div className="star"></div>
-        <div className="star"></div>
-        <div className="star"></div>
-        <div className="star"></div>
-        <div className="star"></div>
-        <div className="star"></div>
-        <div className="star"></div>
-        <div className="star"></div>
-        <div className="star"></div>
-        <div className="star"></div>
-        <div className="star"></div>
-        <div className="star"></div>
-      </div> */}
+            <section className="w-full my-4 " id="project">
+              <Projects />
+            </section>
+            <section className="w-full mt-4" id="resume">
+              <Resume />
+            </section>
+            <section className="w-full mt-8" id="contact">
+              <Contact />
+            </section>
+            <Footer />
+          </>
+        )}
+      </div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,35 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        "move-jump-spin": {
+          "0%": {
+            transform: "translateX(-100px) translateY(-50px) rotate(0deg)",
+          },
+          "25%": {
+            transform: "translateX(-50px) translateY(0px) rotate(90deg)",
+          },
+          "50%": {
+            transform: "translateX(0) translateY(-50px) rotate(180deg)",
+          },
+          "75%": {
+            transform: "translateX(50px) translateY(0px) rotate(270deg)",
+          },
+          "100%": {
+            transform: "translateX(100px) translateY(-50px) rotate(360deg)",
+          },
+        },
+        wave: {
+          "0%, 100%": { transform: "translateY(0)" },
+          "50%": { transform: "translateY(-5px)" },
+        },
+      },
+      animation: {
+        "move-jump-spin": "move-jump-spin 1.5s linear infinite alternate",
+        wave: "wave 1.5s ease-in-out infinite",
+      },
+    },
     fontFamily: {
       poppins: ["Poppins"],
       montserrat: ["Montserrat"],
