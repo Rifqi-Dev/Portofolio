7f9f79ae51ab79ca9c142f7b2090c45fd6903a60
Rifqi-Dev <firlianrifqi22@gmail.com>
2024-08-31 18:51:31 +0700

    fix responsive

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -4,9 +4,9 @@ import Sidenav from "../sidenav/Sidenav";
 import "./Layout.css";
 function Layout() {
   return (
-    <div className="flex h-screen ">
+    <div className="flex flex-col md:flex-row h-screen ">
       <Sidenav />
-      <div className="flex-1 min-h-screen overflow-y-scroll">
+      <div className="flex-1 min-h-screen md:overflow-y-scroll">
         <Outlet />
       </div>
     </div>
